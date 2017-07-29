3c2b884a02e6267b7303acebb5922bec1ab590f4
thearmchair <rohit@thearmchair.in>
2017-07-29 23:14:50 +0530

    adding smtp seup for sending emails

---
+++ config/initializers/setup_mail.rb
@@ -0,0 +1,18 @@
+ActionMailer::Base.smtp_settings = {
+  # :address              => "email-smtp.us-east-1.amazonaws.com",
+  # :port                 => 587,
+  # :user_name            => "AKIAIHFOKGMTQSKFPOGA",
+  # :password             => ENV['EMAIL_PASSWORD'],
+  # :authentication       => "plain",
+  # :enable_starttls_auto => true
+  :address              => "smtp.zoho.com",
+  :port                 => 465,
+  :user_name            => Rails.application.secrets.email,
+  :password             => Rails.application.secrets.password,
+  :authentication       => :login,
+  :ssl                  => true,
+  :tls                  => true,
+  :enable_starttls_auto => true 
+}
+
+Mail.register_interceptor(MailInterceptor)
