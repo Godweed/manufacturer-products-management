cb2c45d3f6db2aa2ad3011132c1310287ffdb11d
thearmchair <rohit@thearmchair.in>
2017-07-29 23:47:39 +0530

    adding email creds to secrets.yml file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,8 @@ Rails.application.configure do
     # UNCOMMENT BELOW LINES TO SEND EMAILS
   config.action_mailer.delivery_method = :smtp
 
+  config.action_mailer.default_url_options = { host: "http://sankalpint.herokuapp.com" }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
