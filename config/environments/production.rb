ebb96138547f5ac1f924be35fd2b69cac0ace508
thearmchair <rohit@thearmchair.in>
2017-08-02 01:53:44 +0530

    fixing paperclip default options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,12 +92,12 @@ Rails.application.configure do
       bucket: ENV['S3_BUCKET_NAME'],
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      s3_region: 'ap-south-1',
+      s3_region: 'ap-south-1'
     },
     storage: :s3,
-    s3_headers: { "Cache-Control" => "max-age=31557600" },
     s3_protocol: "http",
     bucket: ENV['S3_BUCKET_NAME'],
     url:  ":s3_domain_url",
+    s3_host_name: "s3.ap-south-1.amazonaws.com"
   }
 end
